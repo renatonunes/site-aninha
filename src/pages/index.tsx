d5efd8acfa527ad772e49a7789418603f3bacf01
renatonunes <reenatonunes@gmail.com>
2023-01-25 23:16:29 -0300

    favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,12 @@ export default function Home() {
           name="description"
           content="Melhor designer de sobrancelhas em Itajaí"
         />
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png"/>
+        <link rel="manifest" href="images/site.webmanifest"></link>
+
       </Head>
 
       <div>
